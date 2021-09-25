3f896e3d58727e8d634176bfd8f368f2d889ff64
michael504-B <91339358+michael504-B@users.noreply.github.com>
2021-09-24 21:27:07 -0500

    Finish

---
+++ Code/SimplePlatformer/Assets/Playermovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    
+    public float MoveSpeed = 3;
+
+    public float JumpHeight = 6;
+
+    public bool isGround = false;
+
+    public float distToGround = 1f;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      float h = Input.GetAxis("Horizontal");
+      float v = Input.GetAxis("Vertical");
+
+      rb.velocity = new Vector3(h * MoveSpeed, rb.velocity.y, v);
+
+
+      if (isGround && Input.GetButtonDown("Jump"))
+      {
+        Jump();
+      }
+
+      GroundCheck();
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, JumpHeight, rb.velocity.z);
+    }
+
+
+    void GroundCheck()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+
+       
+    }
+       
+}
